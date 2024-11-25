df0d551a9cad3b735e394b65eeddd768e355bfa7
TalkingPotato90 <star901210@hanmail.net>
2024-11-25 14:25:49 +0900

    feat: 마이페이지 메인 화면 컴포넌트 구현

--- frontend/src/pages/MyPageMain.jsx
+++ frontend/src/pages/MyPageMain.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import {
   Avatar,
   Box,
   Button,
+  Checkbox,
+  Container,
   FormControl,
   FormControlLabel,
   FormLabel,
@@ -11,19 +14,40 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { styled } from '@mui/material/styles';
 import defaultAvatar from '../assets/defaultAvatar.png';
-import React from 'react';
 
 function MyPageMain() {
+  const [hasPet, setHasPet] = useState(false);
+
+  const handlePetCheckboxChange = (event) => {
+    setHasPet(event.target.checked);
+  };
+
   return (
-    <>
+    <Container>
       {header()}
       {editCategory(defaultAvatar, '회원정보', '회원 정보 수정')}
       {inputDefaultInformation('닉네임', '이명화', '1998-06-03', false)}
-      {editCategory(defaultAvatar, '반려동물', '반려동물 프로필 등록')}
-      {inputDefaultInformation('이름', '돌멩이', '1998-05-13', true)}
+
+      <FormControlLabel
+        control={
+          <Checkbox checked={hasPet} onChange={handlePetCheckboxChange} />
+        }
+        label="반려동물 여부"
+      />
+
+      {hasPet && (
+        <>
+          {editCategory(defaultAvatar, '반려동물', '반려동물 프로필 등록')}
+          {inputDefaultInformation('이름', '돌멩이', '1998-05-13', true)}
+        </>
+      )}
+
+      {introduce()}
       <Button variant="contained">수정</Button>
-    </>
+    </Container>
   );
 }
 
@@ -60,8 +84,8 @@ function inputDefaultInformation(nameLabel, name, birthday, isAnimal) {
           defaultValue={name}
         />
         <TextField
-          disabled
-          id="outlined-disabled"
+          disabled={!isAnimal}
+          id={isAnimal ? 'outlined-helperText' : 'outlined-disabled'}
           label="생년월일"
           defaultValue={birthday}
         />
@@ -73,7 +97,7 @@ function inputDefaultInformation(nameLabel, name, birthday, isAnimal) {
 }
 
 function GenderRadioGroupComponent() {
-  const [value, setValue] = React.useState('female');
+  const [value, setValue] = useState('female');
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -97,7 +121,7 @@ function GenderRadioGroupComponent() {
 }
 
 function NeutralizationRadioGroupComponent() {
-  const [value, setValue] = React.useState('yes');
+  const [value, setValue] = useState('yes');
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -119,6 +143,62 @@ function NeutralizationRadioGroupComponent() {
         <FormControlLabel value="no" control={<Radio />} label="아니오" />
       </RadioGroup>
     </FormControl>
+  );
+}
+
+function introduce() {
+  return (
+    <Box
+      component="form"
+      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="outlined-multiline-static"
+          label="소개 및 특이사항"
+          multiline
+          rows={4}
+          defaultValue="돌멩이는 가만히 있는 것을 좋아합니다."
+        />
+        {InputFileUpload()}
+      </div>
+    </Box>
+  );
+}
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+function InputFileUpload() {
+  return (
+    <>
+      <label>프로필 사진 등록</label>
+      <Button
+        component="label"
+        role={undefined}
+        variant="contained"
+        tabIndex={-1}
+        startIcon={<CloudUploadIcon />}
+      >
+        Upload files
+        <VisuallyHiddenInput
+          type="file"
+          onChange={(event) => event.target.files}
+          multiple
+        />
+      </Button>
+    </>
   );
 }
 
