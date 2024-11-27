80dae60b0128b233aa2299a7da51bcd139a5b393
ljhee92 <weilainib7@gmail.com>
2024-11-27 22:10:36 +0900

    feat: 글 작성 페이지 구현 - react-quill 에디터 적용 - Title 컴포넌트 분리

---
+++ frontend/src/components/ReactQuillEditor.jsx
@@ -0,0 +1,32 @@
+import ReactQuill from 'react-quill';
+import 'react-quill-new/dist/quill.snow.css';
+
+function ReactQuillEditor({ style, value, onChange }) {
+  const modules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, 3, 4, 5, false] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [
+          { list: 'ordered' },
+          { list: 'bullet' },
+          { indent: '-1' },
+          { indent: '+1' },
+        ],
+        [{ color: [] }, { background: [] }],
+        [{ align: [] }, 'link', 'image'],
+        ['clean'],
+      ],
+    },
+  };
+
+  return (
+    <ReactQuill
+      style={style}
+      modules={modules}
+      value={value}
+      onChange={onChange}
+    />
+  );
+}
+export default ReactQuillEditor;
