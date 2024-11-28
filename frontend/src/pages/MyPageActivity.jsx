6031e129363e886b7fb8217adf1d1383d7069a81
ljhee92 <weilainib7@gmail.com>
2024-11-28 17:47:01 +0900

    feat: 마이페이지 작성한 글/댓글 페이지 구현

---
+++ frontend/src/pages/MyPageActivity.jsx
@@ -0,0 +1,267 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  Box,
+  Typography,
+  Stack,
+  Tabs,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Pagination,
+  Paper,
+} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import MuiCard from '@mui/material/Card';
+import PropTypes from 'prop-types';
+
+function MyPageActivity({ myPosts }) {
+  return (
+    <InformationContainer direction="column" justifyContent="space-between">
+      <Card variant="outlined">
+        <Box sx={{ mb: 4 }}>
+          <Header />
+        </Box>
+        <Box sx={{ mb: 4 }}>
+          <NavTabs myPosts={myPosts} />
+        </Box>
+      </Card>
+    </InformationContainer>
+  );
+}
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  width: '100%',
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: 'auto',
+  [theme.breakpoints.up('sm')]: {
+    maxWidth: '1440px',
+  },
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  ...theme.applyStyles('dark', {
+    boxShadow:
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
+}));
+
+const InformationContainer = styled(Stack)(({ theme }) => ({
+  height: '100%',
+  minHeight: '100%',
+  padding: theme.spacing(2),
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(4),
+  },
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage:
+        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    }),
+  },
+}));
+
+function Header() {
+  return (
+    <Box component="section">
+      <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+        작성한 글/댓글
+      </Typography>
+    </Box>
+  );
+}
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function NavTabs({ myPosts }) {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const totalItems = value === 0 ? myPosts.length : 0;
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="글" {...a11yProps(0)} />
+          <Tab label="댓글" {...a11yProps(1)} />
+          <Box
+            sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}
+          >
+            <Typography>총 {totalItems}건</Typography>
+          </Box>
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <MyPostsTable myPosts={myPosts} />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        작성한 댓글 있는 게시물 목록
+      </CustomTabPanel>
+    </Box>
+  );
+}
+
+function MyPostsTable({ myPosts }) {
+  const navigate = useNavigate();
+
+  const columns = [
+    { id: 'no', label: 'NO' },
+    { id: 'board', label: '게시판' },
+    { id: 'title', label: '제목' },
+    { id: 'date', label: '작성일', format: (value) => value.toISOString() },
+  ];
+
+  const rows = myPosts.map((myPost, index) => ({
+    no: index + 1,
+    id: myPost.id,
+    board: myPost.board,
+    title: myPost.title,
+    date: myPost.date,
+  }));
+
+  const [page, setPage] = useState(1);
+  const [rowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  return (
+    <Box>
+      <TableContainer
+        component={Paper}
+        sx={{ maxHeight: 440, border: '1px solid gray', mt: 1 }}
+      >
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align="center"
+                  style={{
+                    fontWeight: 'bold',
+                    borderBottom: '1px solid gray',
+                  }}
+                  sx={{ height: '10px', padding: '7px' }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice((page - 1) * rowsPerPage, page * rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.no}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      if (column.id === 'title') {
+                        return (
+                          <TableCell
+                            key={column.id}
+                            align="center"
+                            sx={{
+                              height: '10px',
+                              padding: '7px',
+                              cursor: 'pointer',
+                            }}
+                            onClick={() =>
+                              navigate(`/community/postDetail/${row.id}`)
+                            }
+                          >
+                            {value}
+                          </TableCell>
+                        );
+                      }
+                      return (
+                        <TableCell
+                          key={column.id}
+                          align="center"
+                          sx={{ height: '10px', padding: '7px' }}
+                        >
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Stack
+        spacing={2}
+        sx={{ display: 'flex', justifyContent: 'center', mt: '16px' }}
+        direction="row"
+      >
+        <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+          <Pagination
+            count={Math.ceil(rows.length / rowsPerPage)} // 페이지 개수를 계산
+            page={page} // 현재 페이지
+            onChange={handleChangePage} // 페이지 변경 함수
+            color="primary"
+            sx={{ display: 'flex', justifyContent: 'center' }}
+          />
+        </Box>
+      </Stack>
+    </Box>
+  );
+}
+
+export default MyPageActivity;
