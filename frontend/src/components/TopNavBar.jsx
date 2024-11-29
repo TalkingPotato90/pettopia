2d61648610ffe5112b2c051d9fbeb663ed4dd744
ljhee92 <weilainib7@gmail.com>
2024-11-29 14:45:50 +0900

    refactor: 숨어 있는 메뉴 삭제

--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import defaultAvatar from '../assets/defaultAvatar.png';
 import ColorModeSelect from '../theme/ColorModeSelect';
 import { SitemarkIcon } from '../pages/CustomIcons';
@@ -20,17 +16,8 @@ const pages = ['홈', '커뮤니티'];
 const userName = '핫도그';
 
 function TopNavBar({ isLoggedIn, onLogout }) {
-  const [anchorElNav, setAnchorElNav] = useState(null);
   const navigate = useNavigate();
   const location = useLocation();
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
   const handleAvatarClick = () => {
     navigate('/mypage/main');
@@ -48,40 +35,6 @@ function TopNavBar({ isLoggedIn, onLogout }) {
           <Link to="/home">
             <SitemarkIcon style={{ width: '24px', height: '24px' }} />
           </Link>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'center',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'center',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: 'center' }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
           <Box
             sx={{
               flexGrow: 1,
@@ -93,7 +46,6 @@ function TopNavBar({ isLoggedIn, onLogout }) {
             {pages.map((page) => (
               <Button
                 key={page}
-                onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: 'white', display: 'block' }}
                 component={Link}
                 to={`/${page === '홈' ? 'home' : 'community/freeboard'}`}
