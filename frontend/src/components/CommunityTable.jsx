586c05b029305bdcd511b1bafa072c42951a4837
TalkingPotato90 <star901210@hanmail.net>
2024-11-29 14:39:57 +0900

    feat: 홈 화면 틀 구현

---
+++ frontend/src/components/CommunityTable.jsx
@@ -0,0 +1,3 @@
+export default function CommunityTable() {
+  return <h1>테이블이 들어 올 자리</h1>;
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import { Stack, styled } from '@mui/material';
+import { Stack, styled, TextField, Button } from '@mui/material';
+import CommunityTable from '../components/CommunityTable';
+import MuiCard from '@mui/material/Card';
 
 const HomeContainer = styled(Stack)(({ theme }) => ({
   height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
@@ -23,10 +25,76 @@ const HomeContainer = styled(Stack)(({ theme }) => ({
   },
 }));
 
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  padding: theme.spacing(2),
+  margin: theme.spacing(2, 0),
+  [theme.breakpoints.up('sm')]: {
+    maxWidth: '450px',
+  },
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  ...theme.applyStyles('dark', {
+    boxShadow:
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
+}));
+
+const LoginCard = styled(Card)(({ theme }) => ({
+  maxWidth: '300px',
+  alignSelf: 'flex-start',
+  padding: theme.spacing(2),
+}));
+
 export default function Home() {
   return (
-    <HomeContainer direction="column" justifyContent="space-between">
-      <h1>Home</h1>
+    <HomeContainer
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={8}
+    >
+      <Stack
+        direction="row"
+        spacing={2}
+        alignItems="center"
+        style={{ width: '100%', maxWidth: '600px' }}
+      >
+        <TextField fullWidth placeholder="검색할 내용" />
+        <Button variant="contained">검색</Button>
+      </Stack>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        spacing={4}
+        style={{ width: '80%' }}
+      >
+        <Card
+          variant="outlined"
+          style={{
+            flexBasis: '70%',
+            maxWidth: '70%',
+            minWidth: 0,
+          }}
+        >
+          <CommunityTable />
+        </Card>
+        <LoginCard
+          variant="outlined"
+          style={{
+            flexBasis: '30%',
+            maxWidth: '30%',
+            minWidth: 0,
+          }}
+        >
+          <Stack direction="column" alignItems="center" spacing={2}>
+            <div>로그인하고 편리하게 이용하세요</div>
+            <Button variant="contained">로그인</Button>
+          </Stack>
+        </LoginCard>
+      </Stack>
     </HomeContainer>
   );
 }
