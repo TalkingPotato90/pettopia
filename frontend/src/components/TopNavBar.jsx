d9756327ae042a8c956506411de21a6dabb5f0c4
ljhee92 <weilainib7@gmail.com>
2024-11-29 15:11:52 +0900

    feat: 상단바 마이페이지 이동 메뉴 추가

--- frontend/src/components/TopNavBar.jsx
+++ frontend/src/components/TopNavBar.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import defaultAvatar from '../assets/defaultAvatar.png';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Container,
+  Button,
+  Menu,
+  MenuItem,
+} from '@mui/material';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 import ColorModeSelect from '../theme/ColorModeSelect';
 import { SitemarkIcon } from '../pages/CustomIcons';
 
@@ -16,11 +19,21 @@ const pages = ['홈', '커뮤니티'];
 const userName = '핫도그';
 
 function TopNavBar({ isLoggedIn, onLogout }) {
+  const [anchorEl, setAnchorEl] = useState(null);
   const navigate = useNavigate();
   const location = useLocation();
 
-  const handleAvatarClick = () => {
-    navigate('/mypage/main');
+  const handleAvatarClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleAvatarClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuClick = (path) => {
+    navigate(path);
+    setAnchorEl(null);
   };
 
   const handleLogout = () => {
@@ -57,14 +70,44 @@ function TopNavBar({ isLoggedIn, onLogout }) {
           <Box sx={{ flexGrow: 0, display: 'flex', alignItems: 'center' }}>
             {isLoggedIn ? (
               <>
-                <Tooltip title="myPage">
-                  <IconButton onClick={handleAvatarClick} sx={{ p: 0 }}>
-                    <Avatar src={defaultAvatar} />
-                  </IconButton>
-                </Tooltip>
+                <IconButton
+                  onClick={handleAvatarClick}
+                  size="large"
+                  edge="start"
+                  sx={{
+                    p: 0,
+                    border: 'none',
+                    backgroundColor: 'transparent',
+                    ml: 2,
+                  }}
+                >
+                  <AccountCircle sx={{ fontSize: 35 }} />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleAvatarClose}
+                >
+                  <MenuItem onClick={() => handleMenuClick('/mypage/main')}>
+                    정보 수정
+                  </MenuItem>
+                  <MenuItem onClick={() => handleMenuClick('/mypage/activity')}>
+                    작성한 글/댓글
+                  </MenuItem>
+                </Menu>
                 <Typography
                   variant="body2"
-                  sx={{ fontWeight: 'bold', color: 'white', ml: 2, mr: 2 }}
+                  sx={{ fontWeight: 'bold', color: 'white', ml: 1, mr: 2 }}
                 >
                   {userName} 님
                 </Typography>
