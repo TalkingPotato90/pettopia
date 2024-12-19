d6d665dba86f221b28043c3a080ff15e5d2fd0cf
ljhee92 <weilainib7@gmail.com>
2024-12-19 14:58:56 +0900

    feat: WebConfig 설정, api 연결 테스트

--- backend/src/main/java/kr/co/pettopia/config/SecurityConfig.java
+++ backend/src/main/java/kr/co/pettopia/config/SecurityConfig.java
@@ -16,10 +16,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
-                request -> request.requestMatchers(PathRequest.toH2Console()).permitAll()
-                        .anyRequest().authenticated())
-                .csrf(csrf -> csrf.ignoringRequestMatchers(PathRequest.toH2Console()))
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
-        return http.cors(Customizer.withDefaults()).build();
+                request -> request
+                        .requestMatchers(PathRequest.toH2Console()).permitAll() // H2 Console 허용
+                        .requestMatchers("/api/**").permitAll() // API 요청 허용
+                        .anyRequest().authenticated()) // 나머지는 인증 필요
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(PathRequest.toH2Console()) // H2 Console CSRF 비활성화
+                        .disable()) // API 요청 CSRF 비활성화
+                .cors(Customizer.withDefaults()) // 기본 CORS 설정
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)); // H2 Console 설정
+        return http.build();
     }
 }
