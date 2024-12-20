20e505b944544e64756e7c4646d51b472a78da03
TalkingPotato90 <star901210@hanmail.net>
2024-12-20 14:44:24 +0900

    fix: 게시글과 카테고리 참조 설정

--- backend/src/main/java/kr/co/pettopia/model/freeboard/domain/Post.java
+++ backend/src/main/java/kr/co/pettopia/model/freeboard/domain/Post.java
@@ -3,6 +3,7 @@ package kr.co.pettopia.model.freeboard.domain;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,8 +33,9 @@ public class Post {
 //    @JoinColumn(name = "USER_ID", referencedColumnName = "USER_ID", foreignKey = @ForeignKey(name = "FK_USER_TO_POST"))
 //    private User user;
 
-    @Column(name = "CATEGORY_ID", nullable = false)
-    private String categoryId;
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "CATEGORY_ID", referencedColumnName = "CATEGORY_ID", foreignKey = @ForeignKey(name = "FK_CATEGORY_TO_POST"))
+    private Category category;
 
     @Column(name="TITLE", length = 100 , nullable = false)
     private String title;
@@ -43,7 +45,7 @@ public class Post {
 
     @CreationTimestamp
     @Column(name= "CREATED_AT", updatable = false)
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     @UpdateTimestamp
     @Column(name="UPDATED_AT")
