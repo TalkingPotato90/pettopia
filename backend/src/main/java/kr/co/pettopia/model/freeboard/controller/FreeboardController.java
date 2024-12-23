0691346258c861153be1c83d8eaf39732d90fbff
TalkingPotato90 <star901210@hanmail.net>
2024-12-23 14:19:36 +0900

    feat: 게시글 DTO 사용하도록 수정

--- backend/src/main/java/kr/co/pettopia/model/freeboard/controller/FreeboardController.java
+++ backend/src/main/java/kr/co/pettopia/model/freeboard/controller/FreeboardController.java
@@ -1,4 +1,24 @@
 package kr.co.pettopia.model.freeboard.controller;
 
+import kr.co.pettopia.model.freeboard.dto.PostDTO;
+import kr.co.pettopia.model.freeboard.service.FreeboardService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/freeboard/posts")
 public class FreeboardController {
+    private final FreeboardService freeboardService;
+
+    public FreeboardController(FreeboardService freeboardService) {
+        this.freeboardService = freeboardService;
+    }
+
+    @GetMapping
+    public List<PostDTO> getAllPosts() {
+        return freeboardService.getAllPosts();
+    }
 }
--- backend/src/main/java/kr/co/pettopia/model/freeboard/service/FreeboardService.java
+++ backend/src/main/java/kr/co/pettopia/model/freeboard/service/FreeboardService.java
@@ -1,9 +1,9 @@
 package kr.co.pettopia.model.freeboard.service;
 
-import kr.co.pettopia.model.freeboard.domain.Post;
+import kr.co.pettopia.model.freeboard.dto.PostDTO;
 
 import java.util.List;
 
 public interface FreeboardService {
-    List<Post> getAllPosts();
+    List<PostDTO> getAllPosts();
 }
