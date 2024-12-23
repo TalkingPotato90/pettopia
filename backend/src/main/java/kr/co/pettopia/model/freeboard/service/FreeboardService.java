22102d939d6da05de093b513e8bc6dbe31aca2bb
TalkingPotato90 <star901210@hanmail.net>
2024-12-23 13:29:50 +0900

    feat: 게시글 전체 조회 기능 구현

--- backend/src/main/java/kr/co/pettopia/model/freeboard/service/FreeboardService.java
+++ backend/src/main/java/kr/co/pettopia/model/freeboard/service/FreeboardService.java
@@ -1,4 +1,9 @@
 package kr.co.pettopia.model.freeboard.service;
 
+import kr.co.pettopia.model.freeboard.domain.Post;
+
+import java.util.List;
+
 public interface FreeboardService {
+    List<Post> getAllPosts();
 }
