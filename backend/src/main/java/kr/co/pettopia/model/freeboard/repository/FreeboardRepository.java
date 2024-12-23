736d2150a6d93095a8a106942074408217ea6190
TalkingPotato90 <star901210@hanmail.net>
2024-12-23 13:29:50 +0900

    feat: 게시글 전체 조회 기능 구현

--- backend/src/main/java/kr/co/pettopia/model/freeboard/repository/FreeboardRepository.java
+++ backend/src/main/java/kr/co/pettopia/model/freeboard/repository/FreeboardRepository.java
@@ -1,4 +1,9 @@
 package kr.co.pettopia.model.freeboard.repository;
 
-public interface FreeboardRepository {
+import kr.co.pettopia.model.freeboard.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FreeboardRepository extends JpaRepository<Post, Integer> {
 }
---
+++ backend/src/main/java/kr/co/pettopia/model/freeboard/service/FreeboardServiceImpl.java
@@ -0,0 +1,21 @@
+package kr.co.pettopia.model.freeboard.service;
+
+import kr.co.pettopia.model.freeboard.domain.Post;
+import kr.co.pettopia.model.freeboard.repository.FreeboardRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FreeboardServiceImpl implements FreeboardService{
+    private final FreeboardRepository freeboardRepository;
+
+    public FreeboardServiceImpl(FreeboardRepository freeboardRepository) {
+        this.freeboardRepository = freeboardRepository;
+    }
+
+    @Override
+    public List<Post> getAllPosts() {
+        return freeboardRepository.findAll();
+    }
+}
