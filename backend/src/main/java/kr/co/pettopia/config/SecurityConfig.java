0f39ad1b148fda96b4090e111260f3d0b5062195
DEV_WOONG <ywcsuwon@gmail.com>
2024-12-23 13:12:04 +0900

    test: 컨트롤러 매핑 테스트

--- backend/src/main/java/kr/co/pettopia/config/SecurityConfig.java
+++ backend/src/main/java/kr/co/pettopia/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,13 +16,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(
+        http
+                .authorizeHttpRequests(
                 request -> request
                         .requestMatchers(PathRequest.toH2Console()).permitAll() // H2 Console 허용
                         .requestMatchers("/api/**").permitAll() // API 요청 허용
+                        .requestMatchers("/oauth/**").permitAll()
                         .anyRequest().authenticated()) // 나머지는 인증 필요
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers(PathRequest.toH2Console()) // H2 Console CSRF 비활성화
+                        .ignoringRequestMatchers(PathRequest.toH2Console())// H2 Console CSRF 비활성화
+                        .ignoringRequestMatchers("/oauth/**")// OAuth CSRF 비활성화
                         .disable()) // API 요청 CSRF 비활성화
                 .cors(Customizer.withDefaults()) // 기본 CORS 설정
                 .headers(headers -> headers
