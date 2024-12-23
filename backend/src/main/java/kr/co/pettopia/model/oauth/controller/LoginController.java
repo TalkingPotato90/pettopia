449ffb40d1547f7f11af57dbe7d34c4d131b61a5
DEV_WOONG <ywcsuwon@gmail.com>
2024-12-23 13:12:04 +0900

    test: 컨트롤러 매핑 테스트

--- backend/src/main/java/kr/co/pettopia/model/oauth/controller/LoginController.java
+++ backend/src/main/java/kr/co/pettopia/model/oauth/controller/LoginController.java
@@ -1,4 +1,15 @@
 package kr.co.pettopia.model.oauth.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class LoginController {
+
+    @GetMapping("/oauth/google/login")
+    public @ResponseBody String OAuth2GoogleLogin() {
+        System.out.println("매핑확인");
+        return "구글로그인 테스트";
+    }
 }
