65e04c530e69f5590e1482558a05d98fe2f4d651
TalkingPotato90 <star901210@hanmail.net>
2024-12-27 13:22:53 +0900

    refactor: 게시글 목록 조회 쿼리 최적화

--- backend/src/main/java/kr/co/pettopia/model/freeboard/repository/FreeboardRepository.java
+++ backend/src/main/java/kr/co/pettopia/model/freeboard/repository/FreeboardRepository.java
@@ -1,9 +1,14 @@
 package kr.co.pettopia.model.freeboard.repository;
 
 import kr.co.pettopia.model.freeboard.domain.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FreeboardRepository extends JpaRepository<Post, Integer> {
+    @EntityGraph(attributePaths = {"users","category"})
+    List<Post> findAll();
 }
