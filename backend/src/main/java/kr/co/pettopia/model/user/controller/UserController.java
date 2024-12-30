19f0f11f7794bcb5d9f412762abdc81fec9a45a3
ljhee92 <weilainib7@gmail.com>
2024-12-30 13:36:10 +0900

    chore: api test 코드 제거

--- backend/src/main/java/kr/co/pettopia/model/user/controller/UserController.java
+++ backend/src/main/java/kr/co/pettopia/model/user/controller/UserController.java
@@ -1,19 +1,7 @@
 package kr.co.pettopia.model.user.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
-@RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
-
-    @GetMapping("/test")
-    public Map<String, String> test() {
-        return Map.of("message", "hello test");
-    }
 }
