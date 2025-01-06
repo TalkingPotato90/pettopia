1d1549eac408e8746ca42d4c00c2c40c3e50302e
ljhee92 <weilainib7@gmail.com>
2025-01-06 16:41:32 +0900

    fix: User, Pet 매핑 수정, mypage/main api 확인

--- backend/src/main/java/kr/co/pettopia/model/user/service/UserServiceImpl.java
+++ backend/src/main/java/kr/co/pettopia/model/user/service/UserServiceImpl.java
@@ -1,9 +1,7 @@
 package kr.co.pettopia.model.user.service;
 
-import kr.co.pettopia.model.user.domain.Pet;
 import kr.co.pettopia.model.user.domain.User;
 import kr.co.pettopia.model.user.dto.MyPageDTO;
-import kr.co.pettopia.model.user.repository.PetRepository;
 import kr.co.pettopia.model.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,12 +12,10 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
-    private final PetRepository petRepository;
 
     @Override
     public MyPageDTO getUserInfo(String userId) {
         Optional<User> user = userRepository.findById(userId);
-        Pet pet = petRepository.findByUser(user.orElseThrow());
-        return MyPageDTO.from(user.orElseThrow(), pet);
+        return MyPageDTO.from(user.orElseThrow(() -> new IllegalArgumentException("not found user")));
     }
 }
