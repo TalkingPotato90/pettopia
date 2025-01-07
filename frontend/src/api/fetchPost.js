2387c6fe768b68b0aae1e1a5956d605f73905a22
DEV_WOONG <ywcsuwon@gmail.com>
2025-01-07 21:02:51 +0900

    refactor: fetchPost.js 수정

--- frontend/src/api/fetchPost.js
+++ frontend/src/api/fetchPost.js
@@ -1,6 +1,6 @@
 export const fetchPost = async () => {
   try {
-    const response = await fetch('/api/v1/freeboard/posts');
+    const response = await fetch('/freeboard/posts');
     if (!response.ok) {
       throw new Error(response.statusText);
     }
