680413124b91668f06f36641cd5a4254ba4db799
TalkingPotato90 <star901210@hanmail.net>
2025-01-07 21:50:29 +0900

    feat: 게시글 작성 서비스 구현

--- backend/src/main/java/kr/co/pettopia/model/freeboard/service/FreeboardService.java
+++ backend/src/main/java/kr/co/pettopia/model/freeboard/service/FreeboardService.java
@@ -1,9 +1,11 @@
 package kr.co.pettopia.model.freeboard.service;
 
 import kr.co.pettopia.model.freeboard.domain.Post;
+import kr.co.pettopia.model.freeboard.dto.CreatePostRequest;
 
 import java.util.List;
 
 public interface FreeboardService {
     List<Post> getAllPosts();
+    Post createPost(CreatePostRequest request);
 }
