9f44fb790fb199fc3e1b453d778296921444a54e
ljhee92 <weilainib7@gmail.com>
2025-01-08 15:53:03 +0900

    fix: 주인 생년월일, 성별 컬럼 제거

--- backend/src/main/java/kr/co/pettopia/model/user/dto/UserInfoResponse.java
+++ backend/src/main/java/kr/co/pettopia/model/user/dto/UserInfoResponse.java
@@ -5,19 +5,16 @@ import kr.co.pettopia.model.user.domain.User;
 
 import java.time.LocalDate;
 
-public record UserInfoResponse(String nickname, LocalDate birthday, char gender,
-                               boolean hasPet, String introduction,
+public record UserInfoResponse(String nickname, boolean hasPet, String introduction,
                                String petName, LocalDate petBirthday, Character petGender, Boolean neutering) {
 
     public static UserInfoResponse from(User user) {
-        return new UserInfoResponse(user.getNickname(), user.getBirthDay(), user.getGender(),
-                user.isHasPet(), user.getIntroduction(),
+        return new UserInfoResponse(user.getNickname(), user.isHasPet(), user.getIntroduction(),
                 null, null, null, null);
     }
 
     public static UserInfoResponse from(User user, Pet pet) {
-        return new UserInfoResponse(user.getNickname(), user.getBirthDay(), user.getGender(),
-                user.isHasPet(), user.getIntroduction(),
+        return new UserInfoResponse(user.getNickname(), user.isHasPet(), user.getIntroduction(),
                 pet.getName(), pet.getBirthday(), pet.getGender(), pet.isNeutering());
     }
 }
