e514015109f42c994f016504aa13e09551d40fe9
DEV_WOONG <ywcsuwon@gmail.com>
2025-01-09 16:38:37 +0900

    fix: 깃 충돌 해결

--- backend/src/main/java/kr/co/pettopia/config/SecurityConfig.java
+++ backend/src/main/java/kr/co/pettopia/config/SecurityConfig.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity(debug = true)
@@ -18,13 +18,17 @@ public class SecurityConfig {
     @Autowired
     private PrincipalOauth2UserService principalOauth2UserService;
 
+    @Autowired
+    private CorsConfig corsConfig;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(corsConfig.corsFilter(), UsernamePasswordAuthenticationFilter.class) // corsFilter 추가
                 .authorizeHttpRequests(
                         request -> request
                                 .requestMatchers(PathRequest.toH2Console()).permitAll() // H2 Console 허용
-                                .requestMatchers("/user/**").permitAll()
+//                        .requestMatchers("/user/**").permitAll()
                                 .requestMatchers("/freeboard/**").permitAll()
                                 .requestMatchers("/oauth/**").permitAll()
                                 .anyRequest().authenticated()) // 나머지는 인증 필요
@@ -60,7 +64,7 @@ public class SecurityConfig {
                         .ignoringRequestMatchers(PathRequest.toH2Console())// H2 Console CSRF 비활성화
                         .ignoringRequestMatchers("/oauth/**")// OAuth CSRF 비활성화
                         .disable()) // API 요청 CSRF 비활성화
-                .cors(Customizer.withDefaults()) // 기본 CORS 설정
+
                 .headers(headers -> headers
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)); // H2 Console 설정
         return http.build();
