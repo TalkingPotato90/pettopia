1b9d4d902e5e04d8d30c93d513075431643d9f32
ljhee92 <weilainib7@gmail.com>
2025-01-20 17:29:49 +0900

    chore: UserInfo -> Profile 수정

--- backend/src/main/java/kr/co/pettopia/model/user/dto/ProfileDTO.java
+++ backend/src/main/java/kr/co/pettopia/model/user/dto/ProfileDTO.java
@@ -6,10 +6,10 @@ public record ProfileDTO (String nickname, boolean hasPet, String introduction,
                           String profileImgUrl, String profileImgBase64,
                           String petName, LocalDate petBirthday, Character petGender, Boolean neutering) {
 
-    public static ProfileDTO of(UserInfoRequest userInfoRequest) {
-        return new ProfileDTO(userInfoRequest.nickname(), userInfoRequest.hasPet(), userInfoRequest.introduction(),
-                userInfoRequest.profileImgUrl(), userInfoRequest.profileImgBase64(),
-                userInfoRequest.petName(), userInfoRequest.petBirthday(),
-                userInfoRequest.petGender(), userInfoRequest.neutering());
+    public static ProfileDTO of(ProfileRequest profileRequest) {
+        return new ProfileDTO(profileRequest.nickname(), profileRequest.hasPet(), profileRequest.introduction(),
+                profileRequest.profileImgUrl(), profileRequest.profileImgBase64(),
+                profileRequest.petName(), profileRequest.petBirthday(),
+                profileRequest.petGender(), profileRequest.neutering());
     }
 }
