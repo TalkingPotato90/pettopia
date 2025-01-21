5a3c6b5c9a3f57f7f8e15b0d6a821a326c75d1fb
DEV_WOONG <ywcsuwon@gmail.com>
2025-01-22 00:10:16 +0900

    feat: 대댓글 삭제 및 댓글 조회 기능 구현

--- frontend/src/pages/PostWrite.jsx
+++ frontend/src/pages/PostWrite.jsx
@@ -15,119 +15,119 @@ import {
 import ReactQuillEditor from '../components/ReactQuillEditor';
 import CommunityBreadCrumbs from '../components/CommunityBreadCrumbs';
 import CommunityTitle from '../components/CommunityTitle';
-import ContainerTheme from "../theme/ContainerTheme";
+import ContainerTheme from '../theme/ContainerTheme';
 
-function PostWrite({user}) {
-    const [content, setContent] = useState(''); // 에디터 내용 관리
-    const [title, setTitle] = useState('');
-    const categoryId = '1';
-    const navigate = useNavigate();
+function PostWrite({ user }) {
+  const [content, setContent] = useState(''); // 에디터 내용 관리
+  const [title, setTitle] = useState('');
+  const categoryId = '1';
+  const navigate = useNavigate();
 
-    const isFormValid = title.trim() !== '' && content.trim() !== '';
+  const isFormValid = title.trim() !== '' && content.trim() !== '';
 
-    const handleRegister = async (e) => {
-        e.preventDefault();
-        if(!isFormValid){
-            alert('제목과 내용을 입력해주세요.');
-            return;
-        }
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    if (!isFormValid) {
+      alert('제목과 내용을 입력해주세요.');
+      return;
+    }
 
-        const postData = {
-            title,
-            content,
-            userId: user.userId,
-            categoryId
-        };
-
-        try{
-            const response = await fetch('/freeboard/posts', {
-                method: 'POST',
-                headers: {
-                    'Content-Type':'application/json',
-                },
-                body: JSON.stringify(postData)
-                }
-            );
-
-            if(response.ok){
-                alert('등록 완료');
-                navigate('/community/freeboard');
-            }else {
-                alert('실패');
-            }
-        }catch (e){
-            console.error(e);
-            alert('대실패')
-        }
+    const postData = {
+      title,
+      content,
+      userId: user.userId,
+      categoryId,
     };
+
+    try {
+      const response = await fetch('/freeboard/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+      });
+
+      if (response.ok) {
+        alert('등록 완료');
+        navigate('/community/freeboard');
+      } else {
+        alert('실패');
+      }
+    } catch (e) {
+      console.error(e);
+      alert('대실패');
+    }
+  };
 
   const handleContentChange = (value) => {
     setContent(value); // 에디터에서 변경된 값 반영
   };
 
   const handleTitleChange = (event) => {
-      setTitle(event.target.value);
+    setTitle(event.target.value);
   };
 
   return (
-      <ContainerTheme direction="column" justifyContent="space-between">
-    <Stack spacing={1}>
-      <CommunityBreadCrumbs />
-      <CommunityTitle />
-      <Box
-        sx={{ display: 'flex', justifyContent: 'flex-start', pt: 2, pl: 10 }}
-      >
-        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-          글 제목
-        </Typography>
-      </Box>
-      <Box
-        component="form"
-        sx={{
-          display: 'flex',
-          justifyContent: 'flex-start',
-          pl: 10,
-          pr: 10,
-          '& > :not(style)': { width: '100%' },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="post-title"
-          variant="outlined"
-          size="small"
-          placeholder="글 제목을 작성해주세요."
-          value={title}
-          onChange={handleTitleChange}
-          autoFocus
-        />
-      </Box>
+    <ContainerTheme direction="column" justifyContent="space-between">
+      <Stack spacing={1}>
+        <CommunityBreadCrumbs />
+        <CommunityTitle />
+        <Box
+          sx={{ display: 'flex', justifyContent: 'flex-start', pt: 2, pl: 10 }}
+        >
+          <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+            글 제목
+          </Typography>
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-start',
+            pl: 10,
+            pr: 10,
+            '& > :not(style)': { width: '100%' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="post-title"
+            variant="outlined"
+            size="small"
+            placeholder="글 제목을 작성해주세요."
+            value={title}
+            onChange={handleTitleChange}
+            autoFocus
+          />
+        </Box>
 
-      <Box
-        sx={{ display: 'flex', justifyContent: 'flex-start', pt: 2, pl: 10 }}
-      >
-        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-          글 내용
-        </Typography>
-      </Box>
-      <Box
-        sx={{ display: 'flex', justifyContent: 'flex-start', pl: 10, pr: 10 }}
-      >
-        <ReactQuillEditor
-          style={{ width: '100%', height: '250px' }} // 원하는 스타일 적용
-          value={content} // 에디터에 바인딩된 값
-          onChange={handleContentChange} // 값 변경 핸들러
-        />
-      </Box>
+        <Box
+          sx={{ display: 'flex', justifyContent: 'flex-start', pt: 2, pl: 10 }}
+        >
+          <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+            글 내용
+          </Typography>
+        </Box>
+        <Box
+          sx={{ display: 'flex', justifyContent: 'flex-start', pl: 10, pr: 10 }}
+        >
+          <ReactQuillEditor
+            style={{ width: '100%', height: '250px' }} // 원하는 스타일 적용
+            value={content} // 에디터에 바인딩된 값
+            onChange={handleContentChange} // 값 변경 핸들러
+          />
+        </Box>
 
-      <Box sx={{ display: 'flex', justifyContent: 'center', pt: 7 }}>
-        <Buttons
-        onRegister={handleRegister}
-        onCancel={() => navigate('/community/freeboard')}/>
-      </Box>
-    </Stack>
-      </ContainerTheme>
+        <Box sx={{ display: 'flex', justifyContent: 'center', pt: 7 }}>
+          <Buttons
+            onRegister={handleRegister}
+            onCancel={() => navigate('/community/freeboard')}
+          />
+        </Box>
+      </Stack>
+    </ContainerTheme>
   );
 }
 
@@ -156,7 +156,9 @@ function Buttons({ onRegister, onCancel }) {
             }}
             variant="contained"
             color={buttonText === '등록' ? 'primary' : 'error'}
-            onClick={buttonText === '등록' ? onRegister : () => setIsModalOpen(true)}
+            onClick={
+              buttonText === '등록' ? onRegister : () => setIsModalOpen(true)
+            }
           >
             {buttonText}
           </Button>
