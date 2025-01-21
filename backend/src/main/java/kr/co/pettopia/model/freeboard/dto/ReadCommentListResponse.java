007a219f8591fbbdf654120e82c215751071b449
DEV_WOONG <ywcsuwon@gmail.com>
2025-01-21 15:53:49 +0900

    feat: 백엔드 댓글 조회 기능 구현

---
+++ backend/src/main/java/kr/co/pettopia/model/freeboard/dto/ReadCommentListResponse.java
@@ -0,0 +1,23 @@
+package kr.co.pettopia.model.freeboard.dto;
+
+import kr.co.pettopia.model.freeboard.domain.Comment;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReadCommentListResponse {
+    private Integer commentId;
+    private Integer postId;
+    private String userId;
+    private String nickname;
+    private String content;
+
+    public ReadCommentListResponse(Comment comment) {
+        this.commentId = comment.getCommentId();
+        this.postId = comment.getPost().getPostId();
+        this.userId = comment.getUser().getUserId();  // User 엔티티에서 userId를 가져옵니다.
+        this.nickname = comment.getNickname();
+        this.content = comment.getContent();
+    }
+}
--- backend/src/main/java/kr/co/pettopia/model/freeboard/repository/CommentRepository.java
+++ backend/src/main/java/kr/co/pettopia/model/freeboard/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package kr.co.pettopia.model.freeboard.repository;
 
 import kr.co.pettopia.model.freeboard.domain.Comment;
+import kr.co.pettopia.model.freeboard.domain.Post;
 import kr.co.pettopia.model.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,4 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
     List<Comment> findByUser(User user);
-}
+    List<Comment> findByPostPostId(Integer postId);}
